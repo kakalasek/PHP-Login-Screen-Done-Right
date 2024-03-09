2dd05392d1a8c2c945b44f5233393a17e613b1c9
Kakalasek <honziccekkj@seznam.cz>
2024-03-09 19:57:29 +0100

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="home-head">
+        <a href=""></a>
+    </div>
+    <div class="home-body">
+
+    </div>
+    <div class="home-foot">
+
+    </div>
+</div>
+</body>
+</html>
