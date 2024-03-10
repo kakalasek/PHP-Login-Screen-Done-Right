ac6853a0dcd0051e3cc9ff1eec1f2d064a01ed94
Kakalasek <honziccekkj@seznam.cz>
2024-03-10 14:59:45 +0100

    database class

---
+++ includes/dbh.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+class Dbh
+{
+    private $host = "localhost";
+    private $dbname = ""; // CHANGE
+    private $dbusername = "root";
+    private $dbpassword = "";
+
+    private static $connection = null;
+
+    protected function __construct(){}
+
+    public static function connect(){
+        if(!self::$connection){
+            try{
+                $pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->dbusername, $this->dbpassword);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$connection = $pdo;
+            } catch(PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            }
+        }
+        return self::$connection;
+    }
+
+    public static function close(){
+        if(self::$connection) {
+            self::$connection = null;
+        }
+    }
+}
---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+   
+    <h3>Register</h3>
+
+    <form action="register.inc.php" method="post">
+        <label for="username">Enter your username:</label>
+        <input id="username" type="text" name="username">
+
+        <label for="passwd">Enter your password:</label>
+        <input id="passwd" type="text" name="passwd">
+
+        <button>Register</button>
+    </form>
+
+</body>
+</html>
